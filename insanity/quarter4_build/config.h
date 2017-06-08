0e7ba6c7a636cd1910e263de173a4e9e88d9625a
ed <3dw4rd@verizon.net>
2017-06-08 14:00:37 -0400

    Remove coin4 binaries

--- insanity/quarter4_build/config.h
+++
@@ -1,8 +0,0 @@
-/* define this to the Quarter major version number */
-#define QUARTER_MAJOR_VERSION 1
-
-/* define this to the Quarter release version number */
-#define QUARTER_MICRO_VERSION 0
-
-/* define this to the Quarter minor version number */
-#define QUARTER_MINOR_VERSION 1
